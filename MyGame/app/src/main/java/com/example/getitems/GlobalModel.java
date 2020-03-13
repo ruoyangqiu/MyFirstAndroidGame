c38e28f7f4efeb96c9b90db438db494a8aa7dcba
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-13 15:37:05 -0700

    Create GlobalModel.java

---
+++ MyGame/app/src/main/java/com/example/getitems/GlobalModel.java
@@ -0,0 +1,12 @@
+package com.example.getitems;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+
+public class GlobalModel {
+    // The app context
+    @SuppressLint("StaticFieldLeak")
+    public static Context MainActivityContext;
+}
